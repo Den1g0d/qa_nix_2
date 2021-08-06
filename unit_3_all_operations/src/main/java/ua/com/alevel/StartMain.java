10fe2968f5651189a5ff4a5866c902d0d7dfa358
Den1g0d <denys.binkovskyi@gmail.com>
2021-08-06 14:25:50 +0300

    third commit

--- unit_3_all_operations/src/main/java/ua/com/alevel/StartMain.java
+++ unit_3_all_operations/src/main/java/ua/com/alevel/StartMain.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class StartMain {
 
     public static void main(String[] args) throws IOException {
+
         System.out.println("unit_3_all_operations_Den1g0d");
         (new ua.com.alevel.UnitThreeTaskOne()).run();
         (new ua.com.alevel.UnitThreeTaskTwo()).run();
